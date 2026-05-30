2734d5c2a61160183047720122eac2c486c7a3b4
sagar_gavand_unlistedasssets <sagar.gavand@unlistedasssets.ai>
2026-05-31 00:14:57 +0530

    changes

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -23,7 +23,7 @@ namespace QuestApiAzure.Controllers
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Date = DateOnly.FromDateTime(DateTime.Now),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
